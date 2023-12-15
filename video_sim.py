30a75734ed1470c239aeb5c48f54eac8b13eb41a
awong234 <aw685@cornell.edu>
2023-12-14 22:17:27 -0500

    update documents

--- video_sim.py
+++ video_sim.py
@@ -70,28 +70,55 @@ Z_flat = gaussian_decay(0.8, 25, grid, 0, 0)
 Z = Z_flat.reshape(128, 128)
 
 # plot
-plt.imshow(Z, origin='lower')
-plt.show()
+# plt.imshow(Z, origin='lower')
+# plt.show()
 
-img = Image.fromarray(Z*255)
-img = img.convert('L')
-img.save('test.png')
+# Pick random directions, speeds, and starting positions
+def sim_blob(NSIM):
+    directions = np.random.uniform(-360, 360, NSIM)
+    speeds     = np.random.uniform(0, 5, NSIM)
+    starting_x = np.random.randint(0, RES, NSIM)
+    starting_y = np.random.randint(0, RES, NSIM)
+    radii      = np.random.uniform(0, 20, NSIM)
+    maxp       = np.random.uniform(0.2, 1.0, NSIM)
+    for i in range(0, NSIM):
+        blob = Blob(
+            starting_position={"x": starting_x[i], "y": starting_y[i]},
+            starting_vector={"direction": directions[i], "speed": speeds[i]},
+            radius = radii[i],
+            max_p = maxp[i]
+        )
+        # Create output folder for blob images
+        name_part_direction = '%.1f' % directions[i]
+        name_part_speed = '%.1f' % speeds[i]
+        name_part_starting_x = '%.1f' % starting_x[i]
+        name_part_starting_y = '%.1f' % starting_y[i]
+        name_part_radius = '%.1f' % radii[i]
+        name_part_maxp = '%.1f' % maxp[i]
+        out_folder = os.path.join(image_dir, f'blob_{i}_direction{name_part_direction}_speed{name_part_speed}_x{name_part_starting_x}_y{name_part_starting_y}_radius{name_part_radius}_maxp{name_part_maxp}')
+        if not os.path.isdir(out_folder):
+            os.mkdir(out_folder)
+        claims = np.array([0 for x in range(0, RES**2)]).reshape(RES**2, 1)
+        for frame in range(0, 30):
+            Z_flat = gaussian_decay(blob.max_p, blob.radius, grid, blob.current_x, blob.current_y)
+            claims += binomial(n=1, p=Z_flat)
+            Z = Z_flat.reshape(RES, RES)
+            # Save blob image
+            img = Image.fromarray(Z*255)
+            img = img.convert('L')
+            img.save(os.path.join(out_folder, f'blob_frame_{frame}.png'))
+            # Save claims image
+            max_claims = np.max(claims)
+            claims_for_image = (claims.reshape(RES, RES) / max_claims) * 255
+            img = Image.fromarray(claims_for_image)
+            img = img.convert('L')
+            img.save(os.path.join(out_folder, f'claims_viz_{frame}.png'))
+            # Save claims _data_ -- this can be kept raw
+            img = Image.fromarray(claims.reshape(RES, RES))
+            img = img.convert('L')
+            img.save(os.path.join(out_folder, f'claims_data_{frame}.png'))
+            blob.advance()
 
-blob = Blob(
-    starting_position={"x": 0, "y": 0},
-    starting_vector={"direction": 45, "speed": 5},
-    radius = 15,
-    max_p = 0.3
-)
-claims = np.array([0 for x in range(0, RES**2)]).reshape(RES**2, 1)
-for i in range(0, 30):
-    Z_flat = gaussian_decay(blob.max_p, blob.radius, grid, blob.current_x, blob.current_y)
-    claims += binomial(n=1, p=Z_flat)
-    Z = Z_flat.reshape(128, 128)
-    img = Image.fromarray(Z*255)
-    img = img.convert('L')
-    # img.save(f'frame_{i}.png')
-    blob.advance()
 
-plt.imshow(claims.reshape(RES, RES), origin='lower')
-plt.show()
+np.random.seed(123)
+sim_blob(1000)
